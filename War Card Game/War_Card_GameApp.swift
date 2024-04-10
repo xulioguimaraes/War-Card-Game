b2238bf68897f6631c69ce216c58ba8c2fea5146
Julio C. Guimarães <despairnatural@gmail.com>
2024-04-10 11:28:37 -0300

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Julio C Guimarães on 10/04/24.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
